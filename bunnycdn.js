cd0ca04228e7fb1ca1377783711c8ebf7bddc8db
Glitch (hello-sqlite) <none>
2020-06-12 15:50:53 +0000

    🍞💏 Checkpoint ./bunnycdn.js:15176294/2741

--- bunnycdn.js
+++ bunnycdn.js
@@ -1,33 +1,47 @@
 let axios = require("axios");
 
 function create_pull_zone(api, name, original_url, type) {
-  const options = {
-    url: "https://bunnycdn.com/api/pullzone",
-    headers: {
-      "Content-Type": "application/json",
+let URL =  'https://bunnycdn.com/api/pullzone';  
+let config = {
+  headers: {
+    "Content-Type": "application/json",
       'Accept': "application/json",
       'AccessKey': api
-    },
-    body: {
-      'Name': name,
-      'Type': type,
-      'OriginUrl': original_url
-    }
-  };
-  
-  equest.post('https://flaviocopes.com/todos', {
-  json: {
-    todo: 'Buy the milk'
   }
-}, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    return
-  }
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(body)
+}
+
+let data = {
+  'Name': name,
+  'Type': type,
+  'OriginUrl': original_url
+}
+axios.post(URL, data, config).then(function(response){
+  console.log(response.data)
+}).catch(function(error){
+  console.log(error.response.data)
 })
+
+}
+
+function delete_pull_zone(api, id) {
+let URL =  'https://bunnycdn.com/api/pullzone/'+ id;  
+let config = {
+  headers: {
+    "Content-Type": "application/json",
+      'Accept': "application/json",
+      'AccessKey': api
+  }
+}
+
+
+axios.post(URL, config).then(function(response){
+  console.log(response.staus)
+}).catch(function(error){
+  console.log(error.response.data)
+})
+
+} 
 
 let api = process.env.API;
 
-create_pull_zone(api, "test", "http://goga.com", 0);
+delete_pull_zone(api,  142672);
