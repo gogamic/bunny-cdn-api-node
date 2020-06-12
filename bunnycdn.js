e57b119dd9bb590c72156808e78ec376c0d20807
Glitch (hello-sqlite) <none>
2020-06-12 15:15:37 +0000

    💏🎎 Checkpoint ./package.json:15176294/162 ./bunnycdn.js:15176294/1168

---
+++ bunnycdn.js
@@ -0,0 +1,34 @@
+let request =  require('request')
+function randomNoGenerator(api, name, original_url, type) {
+  const options = {
+  url: 'https://api.github.com/repos/request/request',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    const info = JSON.parse(body);
+    console.log(info.stargazers_count + " Stars");
+    console.log(info.forks_count + " Forks");
+  }
+}
+  
+request(options, callback);
+  
+  
+  
+  
+  request.post(
+    'http://www.yoursite.com/formpage',
+    { json: { key: 'value' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    }
+);
+  
+  
+}
