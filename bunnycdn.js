b69bf9f462749ccd8731169f90d0e606de26d301
Glitch (hello-sqlite) <none>
2020-06-12 15:37:59 +0000

    ⚽️🏨 Checkpoint ./bunnycdn.js:15176294/1689 ./package.json:15176294/2644

--- bunnycdn.js
+++ bunnycdn.js
@@ -1,39 +1,33 @@
-let request = require("request");
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+let axios = require("axios");
 
 function create_pull_zone(api, name, original_url, type) {
   const options = {
     url: "https://bunnycdn.com/api/pullzone",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
-      AccessKey: api
+      'Accept': "application/json",
+      'AccessKey': api
     },
     body: {
-      Name: name,
-      Type: type,
-      OriginUrl: original_url
+      'Name': name,
+      'Type': type,
+      'OriginUrl': original_url
     }
   };
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const info = JSON.parse(body);
-      console.log(info);
-    } else {
-      console.log(response);
-    }
+  
+  equest.post('https://flaviocopes.com/todos', {
+  json: {
+    todo: 'Buy the milk'
   }
+}, (error, res, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(body)
+})
 
-  request(options, callback);
-}
 let api = process.env.API;
+
 create_pull_zone(api, "test", "http://goga.com", 0);
