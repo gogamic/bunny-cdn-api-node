39d00c548f9e30af2222542c87481abe24cdb8d8
Glitch (bunnny-cdn) <none>
2020-06-13 08:57:03 +0000

    Added more function :)

--- README.md
+++ README.md
@@ -4,9 +4,14 @@ Bunny CDN is the fastest and Cheapest CDN
 
 This is a bunny cdn node.js module for buny cdn API
 
+## How to install
+
+`npm install bunnycdn`
+
 # How to Use IT
 
-As of now there are 4 commands whch are listed below with their patrameters
+These are the all avalaible commands
+
 <br>
 1.create_pull_zone(api, name, original_url, type)
 <br>
@@ -15,27 +20,25 @@ As of now there are 4 commands whch are listed below with their patrameters
 3.purge_cache(api, id)
 <br>
 4.add_host_name(api, id, Hostname)
-
 <!--
 <br>
 5.force_ssl(api, id, Hostname, ssl)
-<br>
-6.get_free_ssl(api, Hostname)
-<br>
-7.billing_info(api, Hostname)
-<br>
-8.add_newblocked_ip(api, pullzoneid, ip)
-<br>
-9.remove_blocked_ip(api, pullzoneid, ip)
 -->
+<br>
+5.get_free_ssl(api, Hostname)
+<br>
+6.billing_info(api, Hostname)
+<br>
+7.add_newblocked_ip(api, pullzoneid, ip)
+<br>
+8.remove_blocked_ip(api, pullzoneid, ip)
 
 # Example
 
 ```
-import { purge_cache } from 'bunnycdn';
+let bunnycdn = require('bunycdn');
 
-purge_cache(api, id)
-
+bunnycdn..delete_pull_zone('Here goes your own parameters')
 ```
 
 # INFO
--- bunnycdn.js
+++ bunnycdn.js
@@ -1,114 +1,249 @@
 let axios = require("axios");
-var request = require('request');
-
-
+var request = require("request");
 
 // create_pull_zone
 function create_pull_zone(api, name, original_url, type) {
-let URL =  'https://bunnycdn.com/api/pullzone';  
-let config = {
-  headers: {
-    "Content-Type": "application/json",
-      'Accept': "application/json",
-      'AccessKey': api
-  }
+  let URL = "https://bunnycdn.com/api/pullzone";
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
+
+  let data = {
+    Name: name,
+    Type: type,
+    OriginUrl: original_url
+  };
+  axios
+    .post(URL, data, config)
+    .then(function(response) {
+      if (response.status == 200) {
+        console.log("host added");
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.data);
+    });
 }
 
-let data = {
-  'Name': name,
-  'Type': type,
-  'OriginUrl': original_url
-}
-axios.post(URL, data, config).then(function(response){
-  if (response.status == 200) {
-    console.log("host added")
-  }
-}).catch(function(error){
-  console.log(error.response.data)
-})
-
-}
-
-// delete
+// delete_pull _zone
 function delete_pull_zone(api, id) {
-request({
-  method: 'DELETE',
-  url: 'https://bunnycdn.com/api/pullzone/'+ id,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'AccessKey': api,
-  }}, function (error, response, body) {
-  console.log('Status:', response.statusCode);
-  console.log('Headers:', JSON.stringify(response.headers));
-  console.log('Response:', body);
-});
-
+  request(
+    {
+      method: "DELETE",
+      url: "https://bunnycdn.com/api/pullzone/" + id,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        AccessKey: api
+      }
+    },
+    function(error, response, body) {
+      console.log("Status:", response.statusCode);
+      console.log("Headers:", JSON.stringify(response.headers));
+      console.log("Response:", body);
+    }
+  );
 }
-
 
 // purge
 function purge_cache(api, id) {
-let URL =  'https://bunnycdn.com/api/pullzone/{id}/purgeCache';  
-let config = {
-  headers: {
-    "Content-Type": "application/json",
-      'Accept': "application/json",
-      'AccessKey': api
-  }
+  let URL = "https://bunnycdn.com/api/pullzone/{id}/purgeCache";
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
+
+  axios
+    .post(URL, config)
+    .then(function(response) {
+      if (response.status == 200) {
+        console.log("sucess");
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.data);
+    });
 }
 
-axios.post(URL, config).then(function(response){
-  if (response.status == 200){
-    console.log('sucess')
-  }
- 
-}).catch(function(error){
-  console.log(error.response.data)
-})
-
-}
-
-// new_host
+// add_new_host
 function add_host_name(api, id, Hostname) {
-let URL =  'https://bunnycdn.com/api/pullzone/addHostname';  
-let config = {
-  headers: {
-    "Content-Type": "application/json",
-      'Accept': "application/json",
-      'AccessKey': api
-  }
+  let URL = "https://bunnycdn.com/api/pullzone/addHostname";
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
+
+  let data = {
+    PullZoneId: id,
+    Hostname: Hostname
+  };
+  axios
+    .post(URL, data, config)
+    .then(function(response) {
+      if (response.status == 204) {
+        console.log("host added");
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.data);
+    });
 }
 
-let data = {
-  "PullZoneId": id,
-  "Hostname": Hostname,
+// delete_a_host
+function delete_host_name(api, id, hostname) {
+  request(
+    {
+      method: "DELETE",
+      url:
+        "https://bunnycdn.com/api/pullzone/deleteHostname?id=" +
+        id +
+        "&hostname=" +
+        hostname,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        AccessKey: api
+      }
+    },
+    function(error, response, body) {
+      console.log("Status:", response.statusCode);
+      if (response.statusCode == 200) {
+        console.log("deleted");
+      } else {
+        console.log("Response:", body);
+      }
+    }
+  );
 }
-axios.post(URL, data, config).then(function(response){
-  if (response.status == 204) {
-    console.log("host added")
-  }
-}).catch(function(error){
-  console.log(error.response.data)
-})
 
+// load_free_ssl
+
+function free_ssl(api, Hostname) {
+  let URL =
+    "https://bunnycdn.com/api/pullzone/loadFreeCertificate?hostname=" +
+    Hostname;
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
+
+  axios
+    .post(URL, config)
+    .then(function(response) {
+      console.log(response.status);
+      if (response.status == 200) {
+        console.log("SSl generated");
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.data);
+    });
 }
 
-module.exports = create_pull_zone;
-module.exports = delete_pull_zone;
-module.exports = purge_cache;
-module.exports = add_host_name;
+// add_new_blockedip
+function new_blocked_ip(api, id, ip) {
+  let URL = "https://bunnycdn.com/api/pullzone/addBlockedIp";
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
 
+  let data = {
+    PullZoneId: id,
+    BlockedIp: ip
+  };
+  axios
+    .post(URL, data, config)
+    .then(function(response) {
+      console.log(response.status);
+      if (response.status == 200) {
+        console.log("host added");
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    });
+}
 
+// remove_blocked_ip
 
+function remove_blocked_ip(api, id, ip) {
+  let URL = "https://bunnycdn.com/api/pullzone/removeBlockedIp";
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
 
+  let data = {
+    PullZoneId: id,
+    BlockedIp: ip
+  };
+  axios
+    .post(URL, data, config)
+    .then(function(response) {
+      console.log(response.status);
+      if (response.status == 200) {
+        console.log("host added");
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    });
+}
 
+// billing
+function get_billing_info(api) {
+  let URL = "https://bunnycdn.com/api/billing";
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      AccessKey: api
+    }
+  };
 
+  axios
+    .get(URL, config)
+    .then(function(response) {
+      console.log(response.status);
+      if (response.status == 200) {
+        console.log(response.data);
+      }
+    })
+    .catch(function(error) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    });
+}
 
-
-
-
-
-
-
-
+// exporting_modules
+module.exports.create_pull_zone = create_pull_zone;
+module.exports.delete_pull_zone = delete_pull_zone;
+module.exports.purge_cache = purge_cache;
+module.exports.add_host_name = add_host_name;
+module.exports.delete_host_name = delete_host_name;
+module.exports.get_free_ssl = free_ssl;
+module.exports.new_blocked_ip = new_blocked_ip;
+module.exports.remove_blocked_ip = remove_blocked_ip;
+module.exports.get_billing_info = get_billing_info;
